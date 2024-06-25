92afd11713f92bdfdaae6e31d1202bc49ac36c21
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-06-25 12:55:14 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Product from "./Components/Products/Product";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Product />
+    </>
+  );
+}
+
+export default App;
